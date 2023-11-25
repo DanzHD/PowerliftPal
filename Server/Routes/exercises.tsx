3dfcb8ccff52ed4f1b5ffd47eba90a785a2e4641
Duongdh <danielhangsengduong@gmail.com>
2023-11-25 15:10:17 +1100

    Set up update for exercise

--- Server/Routes/exercises.tsx
+++ Server/Routes/exercises.tsx
@@ -44,4 +44,28 @@ router.post('/exercise/delete', async (req, res) => {
    }
 });
 
+router.post('/exercise/update', async (req, res) => {
+    const {workoutid, name, intensity} = req.body;
+    const username = req.user.username;
+
+    try {
+        if (!req.isAuthenticated()) {
+            res.sendStatus(403);
+        }
+
+        await db.query(`UPDATE exercise SET
+            intensity = COALESCE(NULLIF('${intensity}', 'undefined'), '${intensity}')::smallint
+            WHERE 
+            workoutid = ${workoutid} AND
+            name = '${name}' AND 
+            username = '${username}'
+        `);
+        res.sendStatus(200);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
+    }
+})
+
 module.exports = router;
