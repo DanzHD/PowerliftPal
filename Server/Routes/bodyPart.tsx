6b86e7fcc52b9078ad4aed63d54aeaf1444f8ffd
Duongdh <danielhangsengduong@gmail.com>
2023-11-25 11:42:27 +1100

    Added read to bodypart

--- Server/Routes/bodyPart.tsx
+++ Server/Routes/bodyPart.tsx
@@ -6,6 +6,10 @@ router.post('/bodyPart/create', async (req, res) => {
     const { muscleGroup: newMuscleGroup} = req.body;
 
     try {
+        if (!req.isAuthenticated()) {
+            res.sendStatus(403);
+        }
+
         await db.query(`INSERT INTO bodypart 
             VALUES ('${newMuscleGroup}')
         `);
@@ -16,5 +20,19 @@ router.post('/bodyPart/create', async (req, res) => {
     }
 
 })
+
+router.get('/bodypart/readAll', async (req, res) => {
+   try {
+       if (!req.isAuthenticated()) {
+           res.sendStatus(403);
+       }
+
+       let muscleGroups = await db.query(`SELECT * FROM bodypart`);
+       res.json(muscleGroups);
+   } catch(error) {
+       console.log(error);
+       res.sendStatus(400);
+   }
+});
 
 module.exports = router;
