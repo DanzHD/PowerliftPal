9fd5fdc73a1389e1e56b0303e4d45edf70045d59
Duongdh <danielhangsengduong@gmail.com>
2023-11-25 14:39:28 +1100

    Added ability to create exercise

--- Server/Routes/exercises.tsx
+++ Server/Routes/exercises.tsx
@@ -2,4 +2,23 @@ const db = require('../utils/Database.tsx');
 const express = require('express');
 const router = express.Router();
 
+router.use('/exercise/create', async (req, res) => {
+    const { workoutid, name, intensity} = req.body;
+    const username = req.user.username;
+
+    try {
+        if (!req.isAuthenticated()) {
+            res.sendStatus(403);
+        }
+
+        await db.query(`INSERT INTO exercise
+            VALUES (${workoutid}, '${name}', ${intensity}, '${username}')
+        `)
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
+    }
+});
+
 module.exports = router;
