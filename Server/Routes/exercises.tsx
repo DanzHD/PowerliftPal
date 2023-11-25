e3d72d7a52fe68e958ba2d2e92d7a729a83f7b53
Duongdh <danielhangsengduong@gmail.com>
2023-11-25 14:49:09 +1100

    Added ability to delete exercise

--- Server/Routes/exercises.tsx
+++ Server/Routes/exercises.tsx
@@ -2,8 +2,8 @@ const db = require('../utils/Database.tsx');
 const express = require('express');
 const router = express.Router();
 
-router.use('/exercise/create', async (req, res) => {
-    const { workoutid, name, intensity} = req.body;
+router.post('/exercise/create', async (req, res) => {
+    const { workoutid, name, intensity } = req.body;
     const username = req.user.username;
 
     try {
@@ -19,6 +19,29 @@ router.use('/exercise/create', async (req, res) => {
         console.log(error);
         res.sendStatus(400);
     }
+});
+
+router.post('/exercise/delete', async (req, res) => {
+   const {workoutid, name} = req.body;
+   const username = req.user.username;
+
+   try {
+       if (!req.isAuthenticated()) {
+           res.sendStatus(403);
+       }
+
+       await db.query(`DELETE FROM exercise
+           WHERE 
+           workoutid = ${workoutid} AND
+           name = '${name}' AND
+           username = '${username}'
+       `)
+       res.sendStatus(200);
+
+   } catch (error) {
+       console.log(error);
+       res.sendStatus(400);
+   }
 });
 
 module.exports = router;
