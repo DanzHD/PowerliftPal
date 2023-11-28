96e07da63af134153f4daf276993e99ae25becd6
Duongdh <danielhangsengduong@gmail.com>
2023-11-28 11:08:24 +1100

    Folder setup

---
+++ Client/src/App.tsx
@@ -0,0 +1,16 @@
+import './Styles/_index.scss'
+import './Styles/_typography.scss'
+import Dashboard from "./Pages/Main/Dashboard/Dashboard.tsx";
+
+function App() {
+
+
+    return (
+        <>
+            <Dashboard />
+
+        </>
+    )
+}
+
+export default App
